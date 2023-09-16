55f685c893564ee347972b17cedb21e8a543ea00
Pis4ni <pis4wrk@gmail.com>
2023-09-16 15:35:40 +0200

    add: all basic components

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
+import AppCardDisplay from "./components/AppCardDisplay.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppSearchbar from "./components/AppSearchbar.vue";
 
 export default {
   data() {
     return {
       title: "Hello world"
     }
-  }
-
-  // components: {
-  //   MyComponent,
-  // },
+  },
+  components: {AppCardDisplay,AppHeader,AppSearchbar},
 };
 </script>
 
 <template>
   <h1>{{ title }}</h1>
+  <AppHeader />
+  <AppSearchbar />
+  <AppCardDisplay />
 </template>
 
 <style lang="scss"></style>
